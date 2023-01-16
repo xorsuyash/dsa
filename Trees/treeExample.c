b29fa9ce767f0c7ec2d5d512008afc97b0e14ae3
xorsuyash <suyashsrivastavam87@gmail.com>
2023-01-16 23:52:05 +0530

    added tree preorder traversal

---
+++ Trees/treeExample.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct treenode{
+  int value;
+  struct treenode * left;
+  struct treenode * right;
+}treenode;
+
+treenode * createnode(int value)
+{
+    treenode * result=malloc(sizeof(treenode));
+    if(result != NULL)
+    {
+        result->left=NULL;
+        result->right=NULL;
+        result->value=value;
+    }
+    return result;
+}
+
+void printtree(treenode *root)
+{
+    if(root == NULL)
+    {
+        printf("---<empty>---\n");
+        
+    }
+    printf("value =%d\n",root->value);
+    printf("left\n");
+    printf(root->left);
+    printf("right\n");
+    printf(root->right);
+    printf("done\n");
+
+}
+
+int main()
+{
+   treenode *n1=createnode(10);
+   treenode * n2=createnode(30);
+   treenode * n3=createnode(45);
+   treenode * n4=createnode(56);
+   treenode * n5= createnode(60);
+
+   n1->left=n2;
+   n1->right=n3;
+   n3->left=n4;
+   n3->right=n5;
+   
+
+   printtree(n1);
+
+
+      
+}
